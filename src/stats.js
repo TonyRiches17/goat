b043aa8989fd77e868f8fa70e7a42652dce855c5
Rich <TonyRiches17@yahoo.com>
2026-03-23 07:57:55 -0400

    Start Stats

---
+++ src/stats.js
@@ -0,0 +1,15 @@
+const stats = [
+  {
+    "lebron james": {
+      ppg: 27,
+      rpg: 7,
+      apg: 7,
+      spg: 1.5,
+      tpg: 2.2,
+      tpts: 43000,
+      treb: 11000,
+      tast: 11000,
+      tstl: 4000,
+    }
+  }
+]
